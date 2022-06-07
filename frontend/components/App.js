adf54c7ae79d039253784cadf6a65318843232b8
justinrivera47 <justinrivera47@gmail.com>
2022-06-07 12:26:58 -0400

    Rendered todos on the app. I also connected the submit function to the form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  state = {
+    todos: [],
+    message: '',
+  }
+
+  fetchTodo = () => {
+    axios.get(URL)
+    .then(res => {
+      console.log(res.data.data)
+      this.setState({
+        ...this.state,
+        todos: res.data.data,
+        message: res.data.message
+      })
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        message: err.response.data.message
+      })
+    })
+  }
+
+  componentDidMount = () => {
+    this.fetchTodo()
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h2>{this.state.message}</h2>
+        <ul>
+          {
+            this.state.todos.map((todo) => {
+              return <li key={todo.id}>{todo.name}{todo.completed ? <span> ✓</span> : <span></span>}</li>
+            })
+          }
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <input />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
   }
 }
